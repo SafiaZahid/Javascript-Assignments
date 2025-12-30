9b2ea90e3ca47a39d7775ad03a59d155ab7b3ec5
Safia Zahid <safiaxahid@gmail.com>
2025-12-30 19:16:45 +0500

    Fix Chapter 4 folder (remove nested git repo)

---
+++ Chapter4/Task 1 & 2/app.js
@@ -0,0 +1,21 @@
+var visitorName = "John Doe";
+var productTitle = "T-Shirts";
+var quantity = 5;
+
+document.write (visitorName + " ordered " + quantity +" " + productTitle + " on XYZ Clothing Store.");
+
+//  Task-2
+// Legal Variables:
+// 1. myVar
+// 2. age
+// 3. user_Name
+// 4. visitCount
+// 5. email
+
+// Illegal Variables:
+// 1. 12rose
+// 2. alert
+// 3. var
+// 4. user name
+// 5. @age
+
---
+++ Chapter4/Task3/app.js
@@ -0,0 +1,6 @@
+
+document.write("a: Variables name can only contain letters, numbers, $, and _. For example $my_1stVariable <br>")
+document.write("b: Variables must begin with a letters, $, or _. For example $name, _name or name <br>")
+document.write("c: Variable names are case sensitive.<br>")
+document.write("d: Variable names should not be JS keywords.")
+
