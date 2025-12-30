025b3176da48c3a222cd0cd73329487cefbf2151
Safia Zahid <safiaxahid@gmail.com>
2025-12-30 19:12:00 +0500

    Fix Chapter 3 folder (remove nested git repo)

---
+++ Chapter3/Task2/app.js
@@ -0,0 +1,3 @@
+var visitCount = 14;
+
+alert ("You have visited this site " + visitCount + " times." );
---
+++ Chapter3/Task4/app.js
@@ -0,0 +1,5 @@
+var visitorName = "John Doe";
+var productTitle = "T-Shirts";
+var Quantity = 5;
+
+document.write (visitorName + " ordered " + Quantity +" " + productTitle + " on XYZ Clothing Store.")
