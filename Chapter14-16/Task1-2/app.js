6de9dc303191be57aeeb281ef89faadeca637788
Safia Zahid <safiaxahid@gmail.com>
2026-01-04 08:21:18 +0500

    Chapter 14-16

---
+++ Chapter14-16/Task1-2/app.js
@@ -0,0 +1,52 @@
+// TASK 1
+
+var students =[];
+console.log (students);
+
+//Task 3
+
+var students = ["safia", "ahmed", "hadia", "zara"];
+console.log (students);
+
+// Task 4
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log (numbers);
+
+// Task 5
+
+var boleans =[true, false];
+console.log (boleans);
+
+// Task 6
+var mixedArray =["safia",true, "ahmed", 1, 2, true, false];
+console.log (mixedArray);
+
+//Task 7
+
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+document.write("<h1>Qualifications:</h1>");
+document.write("<ol>");
+document.write("<li>" +qualifications[0] + "</li>");
+document.write("<li>" +qualifications[1] + "</li>");
+document.write("<li>" +qualifications[2] + "</li>");
+document.write("<li>" +qualifications[3] + "</li>");
+document.write("<li>" +qualifications[4] + "</li>");
+document.write("<li>" +qualifications[5] + "</li>");
+document.write("<li>" +qualifications[6] + "</li>");
+document.write("<li>" +qualifications[7] + "</li>");
+document.write("</ol>");
+
+
+//Task 8
+
+var students =["Micheal", "John", "Tony",];
+var scores =[320, 230, 480];
+var totalMarks = 500;
+var percentage;
+
+document.write ("<h3> Score of Micheal is " +scores[0] + ". Percentage : " + (scores[0] / totalMarks) * 100 + "%</h3>");
+
+document.write ("<h3> Score of John is " +scores[1] + ". Percentage : " + (scores[1] / totalMarks) * 100 + "%</h3>");
+
+document.write ("<h3> Score of Tony is " +scores[2] + ". Percentage : " + (scores[2] / totalMarks) * 100 + "%</h3>");
